b0a6b7fc0f31c88737b28a3f49c29059d64b3c50
Luan Argolo <ipsluan@icloud.com>
2022-02-06 12:21:46 -0300

    Finalizar a parte visual do projeto

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import "./App.css";
+
 function App() {
   return (
-    <div>
+    <div className="app">
       <h1>Calculadora IMC</h1>
+      <span>Vamos calcular seu imc</span>
+      <div className="app-input">
+        <input type="text" placeholder="Peso em kg - Ex: 90" />
+        <input type="text" placeholder="Altura em cm - Ex: 189" />
+        <button>Calcular</button>
+      </div>
+      <h2>Seu imc foi 25, você está abaixo do peso!</h2>
     </div>
   );
 }
